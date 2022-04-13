7c05a2e2f4cabd6d5cb8d42abb5fdf6df73cfa79
monotic1301 <42168581+monotic1301@users.noreply.github.com>
2022-04-13 20:45:08 +0900

    Update 보물.py

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,9 +1,22 @@
 N = int(input())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
+
 A.sort()
-B.sort(reverse=True)
 sum = 0
+used = []
+print(A)
+for _ in range(len(A)):
+    imax, idx = -1, 0
+    print(used)
+    for j in range(len(B)):
+        if j in used:
+            continue
+        elif B[j] > imax:
+            imax = B[j]
+            idx = j
+    used.append(idx)
+print(used)
 for i in range(N):
-    sum += A[i]*B[i]
+    sum += A[i] * B[used[i]]
 print(sum)
